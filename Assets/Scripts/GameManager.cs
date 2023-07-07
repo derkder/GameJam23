31d38cd712da4969fb783696d0130dcedbe661bc
derkder <1026305391@qq.com>
2023-07-07 22:42:39 +0800

    feat: 调价静态单例GameManager管理全局

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    /// <summary>
+    /// 切换Scene
+    /// </summary>
+    /// <param 场景名字name="sceneName"></param>
+    public void SceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
