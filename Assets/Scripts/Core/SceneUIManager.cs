a1cc8df76ddc4ab5e245666ec204a28db77fe3e4
derkder <1026305391@qq.com>
2023-07-09 05:22:41 +0800

    feat: ui界面事件接口

--- Assets/Scripts/Core/SceneUIManager.cs
+++ Assets/Scripts/Core/SceneUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Linq;
 using System.Xml.Serialization;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.Gameplay.Manager
@@ -13,21 +14,46 @@ namespace Assets.Scripts.Gameplay.Manager
         public event Action OnPauseLevel;
         public event Action OnRetryLevel;
         public event Action OnResumeLevel;
+        public event Action OnStartBall;
+        public event Action OnStopBall;
 
         public Transform _pnlMain;
         public Transform _pnlRightCorner;
         public Slider BulleSlider;
+        //public TextMeshProUGUI TextScore;
+
+        private Button _btnPlay;
 
         private void Awake() {
             instance = this;
-
+            //_btnPlay = gameObject.Find("BtnPlay");
             _pnlMain.gameObject.SetActive(true);
             _pnlRightCorner.gameObject.SetActive(false);
             BulleSlider.gameObject.SetActive(true);
         }
 
+        public void SwitchBulletTimeEffect(bool isEnabled)
+        {
+            GravityManager.instance.ball.SwitchTrajectoryState(isEnabled);
+            if (isEnabled)
+            {
+                GameManager.Instance.MainCamera.GetComponent<BlurEffect>().intensity = 0.4f;
+            }
+            else
+            {
+                GameManager.Instance.MainCamera.GetComponent<BlurEffect>().intensity = 0.0f;
+            }
+        }
+
+        #region 暂停开始界面
+        public void Play()
+        {
+            OnStartBall?.Invoke();
+        }
+
         public void Pause()
         {
+            OnStopBall?.Invoke();
             OnPauseLevel?.Invoke();
             _pnlRightCorner.gameObject.SetActive(true);
             _pnlMain.gameObject.SetActive(false);
@@ -40,12 +66,15 @@ namespace Assets.Scripts.Gameplay.Manager
 
         public void Resume()
         {
+            OnStartBall?.Invoke();
             OnResumeLevel?.Invoke();
             _pnlRightCorner.gameObject.SetActive(true);
             _pnlMain.gameObject.SetActive(false);
         }
+        #endregion
 
-#region 子弹时间进度条
+
+        #region 子弹时间进度条
         /// <summary>
         /// 显示进度条
         /// </summary>
