12042ef7f211fb87540826c52aed71edfa7e31ef
QifanYang-bw <qifanyangbw@gmail.com>
2023-07-10 17:12:44 +0800

    feat: switch to rk method

--- Assets/Scripts/Gameplay/Objects/Ball.cs
+++ Assets/Scripts/Gameplay/Objects/Ball.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 using static UnityEngine.Networking.UnityWebRequest;
+using UnityEngine.Apple.ReplayKit;
+using UnityEditor;
 
 namespace Assets.Scripts {
     [Serializable]
@@ -23,6 +25,26 @@ namespace Assets.Scripts {
         public Vector3 accel;
         public bool isTrajectoryOn;
 
+        public const float oneSixth = 1 / 6f;
+
+        public delegate Vector2 acceletationDelegate(Vector2 pos);
+        public delegate Vector4 rawUpdateDelegate(Vector4 state, float dt, Vector2 accel);
+
+        private Vector4 rawUpdate(Vector4 state, float dt, Vector2 accel) {
+            Vector2 dv = accel * dt;
+            Vector2 dp = new Vector2(state.z, state.w) * dt;
+            return new Vector4(dp.x, dp.y, dv.x, dv.y);
+        }
+
+        private Vector4 rkUpdate(rawUpdateDelegate fUpdate, Vector4 state, float dt, acceletationDelegate GetAccel) {
+            Vector2 accel = GetAccel(state);
+            Vector4 k1 = fUpdate(state, dt, accel);
+            Vector4 k2 = fUpdate(state + dt * k1 * .5f, dt, accel);
+            Vector4 k3 = fUpdate(state + dt * k2 * .5f, dt, accel);
+            Vector4 k4 = fUpdate(state + dt * k3, dt, accel);
+            return oneSixth * (k1 + k2 * 2 + k3 * 2 + k4);
+        }
+
         private void Start() {
             speed = initialSpeed;
 
@@ -32,31 +54,51 @@ namespace Assets.Scripts {
 
             GravityManager.instance.ball = this;
             GravityManager.instance.ballData = data;
-            AudioManager.Instance.PlaySFX(SfxType.BallRolling, true);
         }
 
         private void FixedUpdate() {
             if (LevelManager.instance.isPaused) {
                 return;
             }
-            accel = GravityManager.instance.GetAcceleration(transform.position);
-        }
 
-        private void Update() {
-            if (LevelManager.instance.isPaused) {
-                return;
+            float multiplier = 100;
+
+            Vector2 pos = transform.position;
+            Vector4 rkState = new Vector4(pos.x, pos.y,
+                                              speed.x, speed.y);
+
+            for (int i = 0; i < multiplier; i++) {
+                float inGameSpeedRatio = GravityManager.instance.speedRatio *
+                    (GravityManager.instance.isBulletTimeOn ? GravityManager.instance.bulletTimeSlowRatio : 1f);
+                float simulatorDt = Time.deltaTime * inGameSpeedRatio;
+
+                Vector4 deltaState = rkUpdate(rawUpdate, rkState, simulatorDt, GravityManager.instance.GetAcceleration);
+                rkState += deltaState;
+                //Debug.LogFormat("rk: {0}", rkState);
             }
-            Vector3 realDeltaSpeed = Time.fixedDeltaTime / ForceCalculator.standardTimeDelta * accel;
-            float inGameSpeedRatio = GravityManager.instance.speedRatio *
-                (GravityManager.instance.isBulletTimeOn ? GravityManager.instance.bulletTimeSlowRatio : 1f);
-            speed = (speed + realDeltaSpeed * inGameSpeedRatio) * (float)GravityManager.instance.damping;
-            transform.localPosition += speed * inGameSpeedRatio;
+            speed = new Vector2(rkState.z, rkState.w);
+            transform.position = rkState;
+
+
+            //accel = GravityManager.instance.GetAcceleration(transform.position);
+            //Vector3 realDeltaSpeed = simulatorDt * accel;
+
+            //speed = (speed + realDeltaSpeed) * (float)GravityManager.instance.damping;
+            //Debug.LogFormat("speed: {0}, realDeltaSpeed: {1}", speed * 1000, realDeltaSpeed * 1000);
+            //transform.localPosition += speed * simulatorDt;
 
             if (isTrajectoryOn) {
                 Vector3[] trajectory = PlotPredictionLine(GravityManager.instance.predictionLineStepCount);
                 GetComponent<LineRenderer>().SetPositions(trajectory);
                 GetComponent<LineRenderer>().positionCount = trajectory.Length;
             }
+        }
+
+        private void Update() {
+            if (LevelManager.instance.isPaused) {
+                return;
+            }
+
 
             Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
             if (!(viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0)) {
@@ -91,9 +133,20 @@ namespace Assets.Scripts {
             int segmentCoverage = GravityManager.instance.predictionLineSegmentCoverage;
             for (int i = 0; i < steps; i++) {
                 for (int j = 0; j < segmentCoverage; j++) {
-                    localAccel = GravityManager.instance.GetAcceleration(localPos);
-                    localSpeed = GetSpeed(localAccel, localSpeed, GravityManager.instance.predictionLineStepRatio);
-                    localPos += GetPositionDelta(localSpeed, GravityManager.instance.predictionLineStepRatio);
+
+                    Vector4 rkState = new Vector4(localPos.x, localPos.y,
+                                                  localSpeed.x, localSpeed.y);
+
+                    float multiplier = 100;
+                    float inGameSpeedRatio = GravityManager.instance.speedRatio *
+                        (GravityManager.instance.isBulletTimeOn ? GravityManager.instance.bulletTimeSlowRatio : 1f);
+                    float simulatorDt = Time.deltaTime * multiplier * inGameSpeedRatio;
+
+                    Vector4 deltaState = rkUpdate(rawUpdate, rkState, simulatorDt, GravityManager.instance.GetAcceleration);
+                    rkState += deltaState;
+
+                    localSpeed = new Vector2(rkState.z, rkState.w);
+                    localPos = rkState;
                 }
                 plotline[i] = localPos;
                 if (GravityManager.instance.IsPositionCollidedWithWell(localPos)) {
